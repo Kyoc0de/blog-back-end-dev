5852b881424ec1a58ad83813fd35c2cdafe28793
kyoo0 <kyoscode@gmail.com>
2020-03-01 09:20:21 -0500

    日志记录 AOP切面拦截

---
+++ src/main/java/com/kyo/blog/aspect/LogAspect.java
@@ -0,0 +1,69 @@
+package com.kyo.blog.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+    @Pointcut("execution(* com.kyo.blog.web.*.*(..))")
+    public void log(){
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+        log.info("Request: {}",requestLog);
+    }
+
+    @After("log()")
+    public void doAfter(){
+//        log.info("after aspect----------");
+    }
+
+    @AfterReturning(returning = "result",pointcut = "log()")
+    public void doAfterReturn(Object result){
+        log.info("Result={}",result);
+    }
+
+//    讲日志记录内容封装成内部类
+private class RequestLog {
+    private String url;
+    private String ip;
+    private String classMethod;
+    private Object[] args;
+
+    public RequestLog(String url, String ip, String classMethod, Object[] args) {
+        this.url = url;
+        this.ip = ip;
+        this.classMethod = classMethod;
+        this.args = args;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "url='" + url + '\'' +
+                ", ip='" + ip + '\'' +
+                ", classMethod='" + classMethod + '\'' +
+                ", args=" + Arrays.toString(args) +
+                '}';
+    }
+}
+}
--- src/main/java/com/kyo/blog/web/IndexController.java
+++ src/main/java/com/kyo/blog/web/IndexController.java
@@ -3,17 +3,19 @@ package com.kyo.blog.web;
 import com.kyo.blog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable  String name){
 //        int i = 9/0;
-        String blog = null;
-        if(blog==null){
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog==null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("running----------");
         return "index";
     }
 }
