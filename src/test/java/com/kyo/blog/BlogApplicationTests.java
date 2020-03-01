105a6e73dda375d2c5c4cdc113115cdf84fb3b87
kyoo0 <kyoscode@gmail.com>
2020-02-29 23:24:43 -0500

    项目初始化

---
+++ src/test/java/com/kyo/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyo.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
