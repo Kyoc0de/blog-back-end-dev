e25b61b35987d2a80e4932ca3ec4a679c902c3bb
kyoo0 <kyoscode@gmail.com>
2020-03-01 10:30:16 -0500

    静态页面thymeleaf适配 引入fragments片段

--- src/main/java/com/kyo/blog/web/IndexController.java
+++ src/main/java/com/kyo/blog/web/IndexController.java
@@ -8,14 +8,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable  String name){
+    @GetMapping("/")
+    public String index(){
 //        int i = 9/0;
 //        String blog = null;
 //        if(blog==null){
 //            throw new NotFoundException("博客不存在");
 //        }
-        System.out.println("running----------");
+//        System.out.println("running----------");
         return "index";
+    }
+
+
+    @GetMapping("/blog")
+    public String blog(){
+//        int i = 9/0;
+//        String blog = null;
+//        if(blog==null){
+//            throw new NotFoundException("博客不存在");
+//        }
+//        System.out.println("running----------");
+        return "blog";
     }
 }
